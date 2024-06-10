e6c4ef49dd200f7197d3439f56846715a8298ec8
Simbledore <95230801+Simbledore@users.noreply.github.com>
2024-06-10 18:59:13 +0200

    removed old edit booking page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,26 @@
-import React, { Fragment } from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Article } from './components/article/Article';
-import { ArticleEdit } from './components/article/ArticleEdit';
-import { Booking } from './components/bookings/Booking';
-import { Box } from '@mui/material';
-import { BookingEdit } from './components/bookings/BookingEdit';
+import React, { Fragment } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Article } from "./components/article/Article";
+import { ArticleEdit } from "./components/article/ArticleEdit";
+import { Booking } from "./components/bookings/Booking";
+import { Box } from "@mui/material";
 
 function App() {
   return (
     <Fragment>
       <Box sx={{ p: 2 }}>
         <Routes>
-          <Route path="/article" element={<Article key={'article'} />} />
+          <Route path="/article" element={<Article key={"article"} />} />
           <Route path="/article/edit/:id" element={<ArticleEdit />} />
-          <Route path="/bookings" element={<Booking key={'bookin'} book_in={true} />} />
-          <Route path="/bookings/out" element={<Booking key={'bookout'} book_in={false}/>} />
-          <Route path='/bookingview/:id' element={<BookingEdit/>} />
+          <Route
+            path="/bookings"
+            element={<Booking key={"bookin"} book_in={true} />}
+          />
+          <Route
+            path="/bookings/out"
+            element={<Booking key={"bookout"} book_in={false} />}
+          />
         </Routes>
       </Box>
     </Fragment>
